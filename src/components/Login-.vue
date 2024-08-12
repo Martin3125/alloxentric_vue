23575fe88a4adde97eab6c5e9aa7959aae433ace
Martin3125 <101206120+Martin3125@users.noreply.github.com>
2024-08-12 00:11:50 -0400

    Se configura la conexion con el servidor donde hay que rehacer las tablas de mongodb

--- src/components/Login-.vue
+++ src/components/Login-.vue
@@ -8,21 +8,21 @@
             <div class="col-md-4 d-flex align-items-center p-0">
                 <div class="card w-100">
                     <div class="card-body">
-                        <form method="post" id="formLogin">
+                        <form method="post" id="formLogin" @submit.prevent="login">
                             <div id="logo_header" class="text-center mb-4">
                                 <img src="C:\Users\Equipo PC\Desktop\Proyecto Vue\alloxentric\src\assets\2.png" alt="logo">
                                 <h2>Alloxentric</h2>
                             </div>
                             <div class="form-group mb-3" id="formCorreo">
                                 <label for="exampleInputEmail1">Correo electrónico</label>
-                                <input name="email" type="email" class="form-control" id="inputCorreo" aria-describedby="emailHelp" placeholder="Ingrese su correo">
+                                <input v-model="email" name="email" type="email" class="form-control" id="inputCorreo" aria-describedby="emailHelp" placeholder="Ingrese su correo">
                             </div>
                             <div class="form-group mb-3" id="formContraseña">
                                 <label for="exampleInputPassword1">Contraseña</label>
-                                <input name="password" type="password" class="form-control" id="inputContraseña" placeholder="Ingrese su contraseña">
+                                <input v-model="pwd" name="pwd" type="password" class="form-control" id="inputContraseña" placeholder="Ingrese su contraseña">
                             </div>
                             <!-- <button type="submit" class="btn btn-primary w-100 mb-2" id="btnIniciarSesion" >Iniciar sesión</button> -->
-                            <a class="btn btn-primary w-100 mb-2" id="btnIniciarSesion" href="/Inicio">Iniciar sesión</a>
+                            <a type="submit" class="btn btn-primary w-100 mb-2" id="btnIniciarSesion" href="/Inicio">Iniciar sesión</a>
                             <a class="btn btn-secondary w-100" id="btnRegistrarse" href="/Registro">Registrarse</a>
                             <label for="" style="text-align: center; color: red;"></label>
                         </form>
@@ -33,12 +33,39 @@
     </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'Login-',
-  // Definición del componente
+  name: 'Login-',// Definición del componente
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post('http://localhost:5000/api/login', {
+          email: this.email,
+          password: this.password
+        });
+        // Manejar la respuesta según lo necesario, por ejemplo, redirigir al usuario
+        if (response.data.success) {
+          this.$router.push('/Inicio');
+        } else {
+          alert('Inicio de sesión fallido');
+        }
+      } catch (error) {
+        console.error('Error al iniciar sesión:', error);
+      }
+    }
+  }
 }
 </script>
   
 <style>
 
 </style>
+ 
+  
--- src/components/Registro-.vue
+++ src/components/Registro-.vue
@@ -8,31 +8,31 @@
             <div class="col-md-4 d-flex align-items-center p-0">
                 <div class="card w-100">
                     <div class="card-body">
-                        <form method="post" id="formLogin">
+                        <form method="post" id="formLogin2" @submit.prevent="registerUser">
                             <div id="logo_header" class="text-center mb-4">
                                 <img src="C:\Users\Equipo PC\Desktop\Proyecto Vue\alloxentric\src\assets\2.png" alt="logo">
                                 <h2>Alloxentric</h2>
                             </div>
                             <div class="form-group mb-3" id="formCorreo">
                                 <label for="exampleInputEmail1">Nombre</label>
-                                <input name="email" type="email" class="form-control" id="inputCorreo" aria-describedby="emailHelp" placeholder="Ingrese su Nombre">
+                                <input v-model="nombre" name="nombre" type="nombre" class="form-control" id="inputCorreo" aria-describedby="emailHelp" placeholder="Ingrese su Nombre">
                             </div>
                             <div class="form-group mb-3" id="formCorreo">
                                 <label for="exampleInputEmail1">Correo electrónico</label>
-                                <input name="email" type="email" class="form-control" id="inputCorreo" aria-describedby="emailHelp" placeholder="Ingrese su correo">
+                                <input v-model="email" name="email" type="email" class="form-control" id="inputCorreo" aria-describedby="emailHelp" placeholder="Ingrese su correo">
                             </div>
                             <div class="form-group mb-3" id="formContraseña">
                                 <label for="exampleInputPassword1">Contraseña</label>
-                                <input name="password" type="password" class="form-control" id="inputContraseña" placeholder="Ingrese su contraseña">
+                                <input v-model="pwd" name="pwd" type="password" class="form-control" id="inputContraseña" placeholder="Ingrese su contraseña">
                             </div>
                             <div class="form-group mb-3" id="formContraseña">
                                 <label for="exampleInputPassword1">Repetir Contraseña</label>
-                                <input name="password" type="password" class="form-control" id="inputContraseña" placeholder="Repita su contraseña">
+                                <input v-model="passwordConfirm" name="password" type="password" class="form-control" id="inputContraseña" placeholder="Repita su contraseña">
                             </div>
                             <!-- <button type="submit" class="btn btn-primary w-100 mb-2" id="btnIniciarSesion" >Iniciar sesión</button> -->
-                            <a  class="btn btn-primary w-100 mb-2" id="btnRegistrarse" href="/Login">Registrar</a>
-                            <a  class="btn btn-secondary w-100" id="btnRegistrarse" href="/Login">Volver</a>
-                            <label for="" style="text-align: center; color: red;"></label>
+                            <button  type="submit" class="btn btn-primary w-100 mb-2" id="btnRegistrarse">Registrar</button>
+                            <a  class="btn btn-secondary w-100" id="btnRegistrarse" href="/">Volver</a>
+                            <label  v-if="errorMessage" style="text-align: center; color: red;">{{ errorMessage }}</label>
                         </form>
                     </div>
                 </div>
@@ -40,13 +40,52 @@
         </div>
     </div>
 </template>
+
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Registro-',
-  // Definición del componente
+  data() {
+    return {
+      nombre: '',
+      email: '',
+      pwd: '',
+      passwordConfirm: '',
+      errorMessage: ''
+    };
+  },
+  methods: {
+    async registerUser() {
+      // Verificar que las contraseñas coincidan
+      if (this.pwd !== this.passwordConfirm) {
+        this.errorMessage = 'Las contraseñas no coinciden';
+        return;
+      }
+
+      try {
+        const response = await axios.post('http://localhost:3000/api/register', {
+          nombre: this.nombre,
+          email: this.email,
+          pwd: this.pwd
+        });
+
+        // Redirigir al usuario a la página de Login después del registro exitoso
+        if (response.data.success) {
+          this.$router.push('/');
+        } else {
+          this.errorMessage = response.data.message;
+        }
+      } catch (error) {
+        this.errorMessage = 'Hubo un error en el registro. Por favor, inténtelo nuevamente.';
+        console.error('Error en el registro:', error);
+      }
+    }
+  }
 }
 </script>
   
 <style>
 
 </style>
+
